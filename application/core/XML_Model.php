3e2b509ad19cde6125a98b30ef58fb8357d3bb91
Daniel <daniel51_1994@hotmail.com>
2017-11-17 11:41:17 -0800

    Complete store feature

--- application/core/XML_Model.php
+++ application/core/XML_Model.php
@@ -3,21 +3,17 @@
  * @author Terence
  */
 
-class XML_Model extends Memory_Model
-{
-        protected $tasks = array();
-        
+class XML_Model extends Memory_Model {
 	/**
 	 * Constructor.
 	 * @param string $origin Filename of the CSV file
 	 * @param string $keyfield  Name of the primary key field
 	 * @param string $entity	Entity name meaningful to the persistence
 	 */
-	function __construct($origin = null, $keyfield = 'id', $entity = null)
-	{
+	function __construct($origin = null, $keyfield = 'id', $entity = null) {
 		parent::__construct();
-                
-                // guess at persistent name if not specified
+
+		// guess at persistent name if not specified
 		if ($origin == null){
 			$this->_origin = get_class($this);
                 }
@@ -25,7 +21,7 @@ class XML_Model extends Memory_Model
 			$this->_origin = $origin;
                 }
                 
-                // remember the other constructor fields
+        // remember the other constructor fields
 		$this->_keyfield = $keyfield;
 		$this->_entity = $entity;
 
@@ -40,58 +36,58 @@ class XML_Model extends Memory_Model
 	 * Load the collection state appropriately, depending on persistence choice.
 	 * OVER-RIDE THIS METHOD in persistence choice implementations
 	 */
-	protected function load()
-	{   
+	protected function load() {
             //load xml
             $tasks = simplexml_load_file($this->_origin);
-                
+
             //load array of field for keys
-            foreach($tasks->children() as $field) {
-                array_push($this->fields, $field);
+            foreach ($tasks->children()->children() as $key => $field) {
+                $this->_fields[] = $key;
             }
-            
+
             //for each task
-            foreach ($tasks->children() as $task){     
+            foreach ($tasks->children() as $task){
                 // build object from a row
                 $record = new stdClass();
                 $record->id = (int)$task->id;
-                $record->task_name = (string)$task->task_name;
+                $record->task = (string)$task->task;
                 $record->priority = (int)$task->priority;
                 $record->size = (int)$task->size;
                 $record->group = (int)$task->group;
                 $record->deadline = (int)$task->deadline;
                 $record->status = (int)$task->status;
                 $record->flag = (string)$task->flag;
-                
+
                 //give data appropriate key and value
                 $key = $record->{$this->_keyfield};
                 $this->_data[$key] = $record;
             }
-                
+
             // --------------------
             // rebuild the keys table
             $this->reindex();
 	}
 
-	/**
-	 * Store the collection state appropriately, depending on persistence choice.
-	 * OVER-RIDE THIS METHOD in persistence choice implementations
-	 */
-        /*
-	protected function store()
-	{
-		// rebuild the keys table
-		$this->reindex();
-		//---------------------
-		if (($handle = fopen($this->_origin, "w")) !== FALSE)
-		{
-			fputcsv($handle, $this->_fields);
-			foreach ($this->_data as $key => $record)
-				fputcsv($handle, array_values((array) $record));
-			fclose($handle);
-		}
-		// --------------------
-	}
-        */
+    /**
+     * Store the collection state appropriately, depending on persistence choice.
+     * OVER-RIDE THIS METHOD in persistence choice implementations
+     */
+    protected function store() {
+        $xmlstr = '<?xml version="1.0" encoding="UTF-8"?><item></item>';
+        // rebuild the keys table
+        $this->reindex();
 
+        if (file_exists($this->_origin)) {
+            $tasks = new SimpleXMLElement($xmlstr);
+
+            foreach($this->_data as $key){
+                $task = $tasks->addChild('item');
+
+                foreach ($key as $property => $value)
+                    $task->addChild($property, $value);
+            }
+
+            $tasks->asXML($this->_origin);
+        }
+    }
 }
